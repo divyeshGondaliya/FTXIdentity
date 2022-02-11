c37abf81aa251824da0698a4dd1f0a34268a7ff2
Divyesh Gondaliya <divyesh.gondaliya@ftxinfotech.com>
2022-02-11 13:08:08 +0530

    change

--- FTX Identity/Resorces/Bioid-GmbH/CaptureViewController.m
+++ FTX Identity/Resorces/Bioid-GmbH/CaptureViewController.m
@@ -1257,7 +1257,7 @@ NSString *const BIOID_FONT = @"HelveticaNeue";
     challengeStep = 0;
     challengeSCNActions = nil;
     challengeSCNActions = [[NSMutableArray alloc] init];
-    
+    //Test
     // Create sequence for 3D Head
     for (id direction in currentChallenge) {
         if([direction isEqualToString:@"up"]) {
